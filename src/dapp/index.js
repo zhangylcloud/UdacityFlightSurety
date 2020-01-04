c3fbd682c5a3b7d49db3680c3c4cf9c64341fd1c
Yunlei <zhangylcloud@outlook.com>
2020-01-04 18:16:58 -0500

    draw UI

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -10,12 +10,56 @@ import './flightsurety.css';
 
     let contract = new Contract('localhost', () => {
 
+        // Handle tab bar switches
+        let airlineSwitch = DOM.elid('airline-tab-switch');
+        let flightSwitch = DOM.elid('flight-tab-switch');
+        let passengerSwitch = DOM.elid('passenger-tab-switch');
+        let insuranceSwitch = DOM.elid('insurance-tab-switch');
+        let statusSwitch = DOM.elid('status-tab-switch');
+
+        let switchList = 
+        [
+            airlineSwitch, 
+            flightSwitch, 
+            passengerSwitch,
+            insuranceSwitch,
+            statusSwitch
+        ]
+
+        let airlineSection = DOM.elid('airline-section');
+        let flightSection = DOM.elid('flight-section');
+        let passengerSection = DOM.elid('passenger-section');
+        let insuranceSection = DOM.elid('insurance-section');
+        let statusSection = DOM.elid('status-section');
+
+        let sectionList = 
+        [
+            airlineSection,
+            flightSection,
+            passengerSection,
+            insuranceSection,
+            statusSection
+        ]
+
+        for(let i = 0; i < switchList.length; ++i){
+            let curSwitch = switchList[i];
+            let curSection = sectionList[i];
+            curSwitch.addEventListener("click", () => {
+                for(let j = 0; j < switchList.length; ++j){
+                    sectionList[j].style.display = "none";
+                }
+                curSection.style.display = "block";
+            });
+        }
+
+         
+
+
         // Read transaction
         contract.isOperational((error, result) => {
             console.log(error,result);
             display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
         });
-    
 
         // User-submitted transaction
         DOM.elid('submit-oracle').addEventListener('click', () => {
